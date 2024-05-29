4e4896c8e6602f119808232222eb7e8ec398522b
Stephen Werronen <stephen.werronen@gmail.com>
2024-05-29 16:00:43 -0700

    Update text router

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 
 module.exports = {
-  development: {
-    client: "mysql2",
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      charset: "utf8"
-    },
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    charset: "utf8",
   },
 };
