675079fecab76d5f159fdbc65190ea9c831e3fbb
Stephen Werronen <stephen.werronen@gmail.com>
2024-06-03 08:17:43 -0700

    Implement JWT token

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const knex = require("knex")(require("../knexfile"));
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const { SECRET_KEY } = process.env;
 
 // helper function
 // todo: refactor to utils
@@ -23,6 +27,12 @@ const isUserAuthenticated = async (req, res, next) => {
     const match = await bcrypt.compare(password, user.password);
 
     if (match) {
+      // add user information for the token payload to the req object
+      req.user = {
+        id: user.id,
+        username: user.user_name
+      }
+
       next();
     } else {
       return res.status(401).json({ message: "Unauthorized" });
@@ -34,9 +44,16 @@ const isUserAuthenticated = async (req, res, next) => {
 };
 
 router.post("/", isUserAuthenticated, (req, res) => {
-  // todo: send response 200 with token
-  // placeholder response
-  res.send("Reached endpoint POST /api/login");
+  const { username, id } = req.user;
+
+  // create the token with username, id in the payload
+  const token = jwt.sign(
+    { username, id },
+    SECRET_KEY
+  );
+
+  // send token to authenticated user
+  res.status(200).json({ token });
 });
 
 module.exports = router;
